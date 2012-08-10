58f7e56b031c00b98b886ba001957d85a201d6b7
Yauhen Dubrouka <yauhen_dubrouka@epam.com>
2012-08-10 09:49:56 +0300

    try with resources

---
+++ src/exceptions/TryResources.java
@@ -0,0 +1,19 @@
+package exceptions;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
+/**
+ * @author Yauhen Dubrouka
+ */
+public class TryResources {
+	public static void main(String[] args) {
+		// BufferedReader is java.lang.AutoCloseable
+		try(BufferedReader br = new BufferedReader(new FileReader("resources/lines.txt"))) {
+			System.out.println(br.readLine());
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+}
