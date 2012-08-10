51c681e9bd9a88bab78ecd09df503d3f8e59c176
Yauhen Dubrouka <yauhen_dubrouka@epam.com>
2012-08-10 11:18:47 +0300

    rename

--- src/exceptions/TryResources.java
+++ src/exceptions/TryResources.java
@@ -10,7 +10,7 @@ import java.io.IOException;
 public class TryResources {
 	public static void main(String[] args) {
 		// BufferedReader is java.lang.AutoCloseable
-		try(BufferedReader br = new BufferedReader(new FileReader("resources/lines.txt"))) {
+		try(BufferedReader br = new BufferedReader(new FileReader("resources/xanadu.txt"))) {
 			System.out.println(br.readLine());
 		} catch (IOException e) {
 			e.printStackTrace();
