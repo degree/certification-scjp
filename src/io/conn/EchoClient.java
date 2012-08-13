b465ccbd08276bc73c1bf6dab31cb7e2d014d5e3
Eugene Dubrovka <eugene.dubrovka@gmail.com>
2012-08-13 17:44:15 +0300

    echo server test

---
+++ src/io/conn/EchoClient.java
@@ -0,0 +1,46 @@
+package io.conn;
+
+/**
+ * @author Yauhen Dubrouka
+ */
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+import java.net.UnknownHostException;
+
+public class EchoClient {
+	public static void main(String[] args) throws IOException {
+
+		Socket echoSocket = null;
+		PrintWriter out = null;
+		BufferedReader in = null;
+
+		try {
+			echoSocket = new Socket("degree.by", 80);
+			out = new PrintWriter(echoSocket.getOutputStream(), true);
+			in = new BufferedReader(new InputStreamReader(echoSocket.getInputStream()));
+		} catch (UnknownHostException e) {
+			System.err.println("Don't know about host: localhost.");
+			System.exit(1);
+		} catch (IOException e) {
+			System.err.println("Couldn't get I/O for the connection to: localhost.");
+			System.exit(1);
+		}
+
+		BufferedReader stdIn = new BufferedReader(new InputStreamReader(System.in));
+		String userInput;
+
+		while ((userInput = stdIn.readLine()) != null) {
+			out.println(userInput);
+			System.out.println("echo: " + in.readLine());
+		}
+
+		out.close();
+		in.close();
+		stdIn.close();
+		echoSocket.close();
+	}
+}
