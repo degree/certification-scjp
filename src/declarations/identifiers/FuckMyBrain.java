bbefaa3cf1a57570b16793241ae2458f6e74305c
Eugene Dubrovka <eugene.dubrovka@gmail.com>
2012-08-20 11:39:08 +0300

    added ternary operator

--- src/declarations/identifiers/FuckMyBrain.java
+++ src/declarations/identifiers/FuckMyBrain.java
@@ -40,5 +40,22 @@ public class FuckMyBrain
 		fuckMyBrain.setМожноПисатьПоРусски("fourty two");
 		System.out.println(fuckMyBrain.getМожноПисатьПоРусски());
 
+		// conditional operator
+		boolean a, b, c, d, e, f, g, h, i, j, k, l;
+		a = true;
+		b = true;
+		c = true;
+		d = true;
+		e = true;
+		f = true;
+		g = true;
+		h = true;
+		i = true;
+		j = true;
+		k = true;
+		l = true;
+		a = b ? c ? d : e ? f : g : h ? i : j ? k : l;
+		a = (/*1>*/b ? (/*2>*/c ? d : (/*3>*/e ? f : g/*<3*/)/*<2*/) : (/*4>*/h ? i : (/*5>*/j ? k : l/*<5*/)/*<4*/)/*<1*/);
+
 	}
 }
